a977436be89a64a9cdfd5812353a69ccb78353bd
Revilo1882 <oliverrharris@gmail.com>
2018-09-27 15:14:28 +0100

    Add fetch data and actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(json => console.log(json))
+	}
+
 	render() {
 		return (
 			<div>
