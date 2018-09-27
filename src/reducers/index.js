f592e2bc43537ba8074fd4342f66cbbe7b22c61e
Revilo1882 <oliverrharris@gmail.com>
2018-09-27 15:18:30 +0100

    Add reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { SET_USERS } from '../actions';
+
+function users(state = {}, action) {
+	switch(action.type) {
+	case SET_USERS:
+		return action.users;
+	default:
+		return state;
+	}
+}
+
+export default users;
